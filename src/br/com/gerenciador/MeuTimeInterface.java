4a349e5573fb25c80b3b5673dda7283dbb3f861e
tainajmedeiros <tainajmedeiros@gmail.com>
2019-09-22 11:03:04 -0300

    feat: add interface MeuTimeInterface

---
+++ src/br/com/gerenciador/MeuTimeInterface.java
@@ -0,0 +1,37 @@
+package br.com.gerenciador;
+
+import br.com.gerenciador.exceptions.TimeNaoEncontradoException;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface MeuTimeInterface {
+
+    public void incluirTime(Long id, String nome, LocalDate dataCriacao, String corUniformePrincipal, String corUniformeSecundario);
+
+    public void incluirJogador(Long id, Long idTime, String nome, LocalDate dataNascimento, Integer nivelHabilidade, BigDecimal salario) throws TimeNaoEncontradoException;
+
+    public void definirCapitao(Long idJogador);
+
+    public Long buscarCapitaoDoTime(Long idTime);
+
+    public String buscarNomeJogador(Long idJogador);
+
+    public String buscarNomeTime(Long idTime);
+
+    public Long buscarJogadorMaiorSalario(Long idTime);
+
+    public BigDecimal buscarSalarioDoJogaodor(Long idJogador);
+
+    public List<Long> buscarJogadoresDoTime(Long idTime);
+
+    public Long buscarMelhorJogadorDoTime(Long idTime);
+
+    public Long buscarJogadorMaisVelho(Long idTime);
+
+    public List<Long> buscarTimes();
+
+    public List<Long> buscarTopJogadores(Integer top);
+}
+
